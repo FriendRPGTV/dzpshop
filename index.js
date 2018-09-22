9d84697a462f69e1cc319fffe41058c22205238c
ChaKung <34345939+FriendRPGTV@users.noreply.github.com>
2018-09-22 08:55:59 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,28 @@ bot.on('message', message => {
     let command = message.content.split(' ')[0];
     command = command.slice(prefix.length);
     var args = message.content.split(' ').slice(1);
+        
+    if(command === 'draw') {
+        var Canvas = require('canvas')
+        , Image = new Canvas.Image
+        , canvas = new Canvas(450, 170)
+        , ctx = canvas.getContext('2d');
+        ctx.font = '30px Impact';
+        let args = message.content.split(" ").slice(1);
+        
+        Image.src = canvas.toBuffer();
+        
+        console.log(Image);
+        ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
+        ctx.fillText(args.join("  "),110, 70);
+        
+        
+        ctx.beginPath();
+        ctx.lineTo(50, 102);
+        ctx.stroke();
+        
+        message.channel.sendFile(canvas.toBuffer());
+    }
     if(command === 'help') {
         const embed = new Discord.RichEmbed()
         .addField('รายการคำสั่ง','**dzp.help** : คำสั่งช่วยเหลือ\n**dzp.status** : ตรวจสอบว่าร้านเปิดหรือปิด\n**dzp.credit** : ตรวจสอบข้อมูลคนสร้างบอท')
