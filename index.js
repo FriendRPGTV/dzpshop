8f5242c64bd6924e7f4878d658240eb11b6f3195
ChaKung <34345939+FriendRPGTV@users.noreply.github.com>
2018-09-29 22:40:00 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ bot.on('message', message => {
     if(command === 'score') {
         let ss = args.join(' ');
         let no = false;
-        let ab = { 'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z' };
+        let ab = [ 'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z' ];
         ab.forEach((txt)=>{
             if (ss.includes(txt)) no = true;
         });
